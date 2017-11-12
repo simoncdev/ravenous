5b5d1a2dda987916d8ed10e7295c186c6a6a170c
Simon <curransimon6@gmail.com>
2017-11-12 16:45:16 +0000

    Ravenous Part IV Draft

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import BusinessList from './components/BusinessList/BusinessList';
 import SearchBar from './components/SearchBar/SearchBar';
+import Yelp from './util/Yelp';
 
+/* emoving hard-coded Business Information Ravenous IV
 const business = {
   imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
   name: 'MarginOtto Pizzeria',
@@ -14,14 +16,31 @@ const business = {
   rating: 4.5,
   reviewCount: 90
 };
+*/
 // eslint-disable-next-line
+/* Removing hard-coded Business Information Ravenous IV
 const businesses = [
   business, business, business, business, business, business
 ];
+*/
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: []
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+    //Apparently could just say this.searchYelp.bind(this); by itself above
+  }
   searchYelp (term, location, sortBy) {
-    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+    Yelp.search(term, location, sortBy).then(
+      businesses => this.setState(
+        {
+          businesses: businesses,
+        }
+      )
+    );
   }
 
   render() {
@@ -29,8 +48,9 @@ class App extends Component {
       <div className="App">
          <h1>ravenous</h1>
          < SearchBar searchYelp={this.searchYelp}/>
-         < BusinessList businesses={businesses}/>
+         < BusinessList business={this.state.businesses}/>
       </div>
+      /*Got rid of businesses={businesses} above*/
     );
   }
 }
---
+++ src/util/Yelp.js
@@ -0,0 +1,71 @@
+const clientId = ['LJqXtIopDYQ8NHrQo1vZeQ']
+const secret = ['4O6TbVdBlLpWMUS0obxQZ9nwh7eYFbww3a04hrl4duLrVyMboWXU3RLLkob9eqdw'];
+const accessToken = {};
+
+let Yelp = {
+  getAccessToken(){
+    if (accessToken){
+      return new Promise(
+        resolve => {
+          resolve(accessToken);
+        }
+        /*Apparently above you don't need the curly brackets - Step 13*/
+      );
+    }
+    /*Not sure about the below interpolation - Step 15*/
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`,
+      {
+        method: 'POST'
+      }
+    ).then(
+      response => {return response.json();}
+    ).then(
+      jsonResponse => {
+        //Changed to this. below because it failed to compile
+        this.accessToken = jsonResponse.access_token;
+      }
+    );
+  },
+
+  search (term, location, sortBy){
+    return Yelp.getAccessToken().then(
+      /*Not sure about this callback gunction with no parameters - Answer: Just use empty ()*/
+      () => {
+        return fetch(
+          `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+          {
+            headers: {
+              Authorization: `Bearer ${accessToken}`
+            }
+          }
+        ).then(
+          response => {return response.json();}
+        ).then(
+          jsonResponse => {
+            if (jsonResponse.businesses){
+              return jsonResponse.businesses.map(
+                /*Not sure whether I should have normal or curly brackets below - step 30*/
+                business => {
+                  return {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.display_address,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories,
+                    rating: business.rating,
+                    reviewCount: business.review_count
+                  }
+                }
+              );
+            }
+          }
+        );
+      }
+    );
+  }
+};
+
+export default Yelp;
